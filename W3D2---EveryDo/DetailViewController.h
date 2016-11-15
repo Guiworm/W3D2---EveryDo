62970eb800cf1767e1df124ba104f64c33dfe530
guiworm <dylan.mccrindle@gmail.com>
2016-11-15 10:56:52 -0500

    Initial Commit

---
+++ W3D2---EveryDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  W3D2---EveryDo
+//
+//  Created by Dylan McCrindle on 2016-11-15.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ W3D2---EveryDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  W3D2---EveryDo
+//
+//  Created by Dylan McCrindle on 2016-11-15.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
